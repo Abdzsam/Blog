b756c42a98d4978bb480ad7bde229133efc76fb4
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-04-27 14:41:49 -0300

    updates

---
+++ src/Travel.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import styled from 'styled-components';
+
+// const SectionTravel = styled.div `
+// #myDiv {
+//   background-color: lightblue;
+//   padding: 20px;
+//   border: 1px solid #ccc;
+//   border-radius: 5px;
+// }
+// `;
+
+export default function Travel(){
+    return(
+        // <SectionTravel>
+            <div id="myDiv">
+                <h2>Travel</h2>
+            </div>
+        // </SectionTravel>
+    );
+}
